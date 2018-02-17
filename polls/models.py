31baf6c13b2fdcfbe963eb2ab549817964e96d97
Azbesciak <witkups@gmail.com>
2018-02-17 10:08:30 +0000

    added models

--- polls/models.py
+++ polls/models.py
@@ -3,4 +3,20 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Device(models.Model):
+    description = models.CharField(max_length=200, null=True, blank=True)
+    
+    
+    def __str__(self):
+        return "id=" + str(self.id) + ", des=" + self.description
+
+class Consumption(models.Model):
+    measure_time = models.DateTimeField('date of measurement')
+    value = models.DecimalField(max_digits=10, decimal_places=5)
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    
+    
+    def __str__(self):
+        return "time=" + str(self.measure_time) +\
+                        str(self.value) +\
+                        ", dev=" + str(self.device)
